8e4876dbd0e283667ed88ff7d276ae8ac3174594
Kyle Kent <kyle.kent@ingka.ikea.com>
2025-04-06 22:15:52 -0400

    begin revamping landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,20 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <p>This is a simple todo list application, with a persistent list using browser storage </p>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+            Nextjs with App Router using React v19
+            {/* <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
-            </code>
+            </code> */}
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            IndexedDb from Minimongo for persistent storage
+          </li>
+          <li className="tracking-[-.01em]">
+            IndexedDb from Minimongo for persistent storage
           </li>
         </ol>
 
